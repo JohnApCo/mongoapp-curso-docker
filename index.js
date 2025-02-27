46687d41223a6bb9d47f8f1e767f566f779309ed
JohnApCo <john.apco.dev@gmail.com>
2025-02-26 20:35:50 -0500

    feat: first commit curso docker

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express()
 mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando chanchitos...')
   const animales = await Animal.find();
   return res.send(animales)
 })
 app.get('/crear', async (_req, res) => {
-  console.log('creando...')
+  console.log('creando chanchito...')
   await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
   return res.send('ok')
 })
